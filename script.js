6ce69dea6f49561776e64027502c7d4ab804fbc6
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-05-24 15:29:06 +0700

    add comment

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  vus: 400,
-  duration: '30s',
+  vus: 400,  //Jumlah user
+  duration: '30s', //durasi selama testing
 };
 export default function () {
   http.get('https://www.google.com/'); // ganti link yang ingin ditest disini
